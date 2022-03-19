00cf1aec76c3a87779d79a028d0c73385c89f0af
Jamaludin Saputra <jamaludinsaputraaa@gmail.com>
2022-03-19 21:54:55 +0700

    coloring background

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+
 var searchButton = document.querySelector(".search-button");
 searchButton.addEventListener('click', function() {
-
+  var inputKeyword = document.querySelector(".input-keyword");
 fetch('https://newsapi.org/v2/top-headlines?country=id&apiKey=599e0889d56045f4bbff7bc47af4dd32' )
 
   .then(response => response.json())
   .then((data) => { 
-    var inputKeyword = document.querySelector(".input-keyword");
+    
     const card = document.querySelector(".row"); 
     let card1 = " ";
-    console.log(inputKeyword);
     data.articles.forEach((d)=>{
         card1=`<div class="col-6 col-md-4 mb-4"> 
                   <div class="card" >
@@ -18,7 +18,7 @@ fetch('https://newsapi.org/v2/top-headlines?country=id&apiKey=599e0889d56045f4bb
                               <h6 class="card-subtitle mb-2 text-muted">${d.description}</h6>
                               <stong> <i> <p class="card-author mb-2 text-muted">${d.author}</p> </i> </stong>
                               <u> <p class="card-publisheadAt mb-2 text-muted">${d.publishedAt}</p> </u>
-                              <a href="#" class="btn  btn-primary btn-tampil" 
+                              <a href="#" class="btn  btn-secondary btn-tampil" 
                               data-bs-toggle="modal" data-bs-target="#detailbutton" data-url="${d.url}">Show details</a>
                           </div>
                     </div>
