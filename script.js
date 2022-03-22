1e37f04b48cd926c3f0be6c6f5fbaeae70ca4da6
Jamaludin Saputra <jamaludinsaputraaa@gmail.com>
2022-03-22 22:32:43 +0700

    live search

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-
 var searchButton = document.querySelector(".search-button");
-searchButton.addEventListener('click', function() {
-  var inputKeyword = document.querySelector(".input-keyword");
-fetch('https://newsapi.org/v2/top-headlines?country=id&apiKey=599e0889d56045f4bbff7bc47af4dd32' )
 
-  .then(response => response.json())
-  .then((data) => { 
+  function search(){
+    var inputKeyword = document.querySelector(".input-keyword").value;
+
+    const contentNews = document.querySelector("#content-news"); 
+
+    contentNews.innerHTML="";
+          fetch("https://newsapi.org/v2/top-headlines?country=id&apiKey=599e0889d56045f4bbff7bc47af4dd32&q=" + inputKeyword)
+
+          .then(response => response.json())
+          .then((data) => { 
     
-    const card = document.querySelector(".row"); 
-    let card1 = " ";
-    data.articles.forEach((d)=>{
-        card1=`<div class="col-6 col-md-4 mb-4"> 
-                  <div class="card" >
-                      <img src="${d.urlToImage}" class="card-img-top" alt="">
-                        <div class="card-body" id="data">
-                              <h5 class="card-title">${d.title}</h5>
-                              <h6 class="card-subtitle mb-2 text-muted">${d.description}</h6>
-                              <stong> <i> <p class="card-author mb-2 text-muted">${d.author}</p> </i> </stong>
-                              <u> <p class="card-publisheadAt mb-2 text-muted">${d.publishedAt}</p> </u>
-                              <a href="#" class="btn  btn-secondary btn-tampil" 
-                              data-bs-toggle="modal" data-bs-target="#detailbutton" data-url="${d.url}">Show details</a>
-                          </div>
-                    </div>
-              </div>`;
-          card.innerHTML += card1;
-   });
- });
-});
+              let card1 = " ";
+              data.articles.forEach((d)=>{
+                  card1=`<div class="col-6 col-md-4 mb-4"> 
+                            <div class="card" >
+                                <img src="${d.urlToImage}" class="card-img-top" alt="">
+                                  <div class="card-body" id="data">
+                                        <h5 class="card-title">${d.title}</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">${d.description}</h6>
+                                        <stong> <i> <p class="card-author mb-2 text-muted">${d.author}</p> </i> </stong>
+                                        <u> <p class="card-publisheadAt mb-2 text-muted">${d.publishedAt}</p> </u>
+                                        <a href="${d.url}" class="btn btn-primary" target="_blank">Show Detail</a>
+                                    </div>
+                              </div>
+                        </div>`;
+                    contentNews.innerHTML += card1;
+            });
+          });
+          }
+search();
